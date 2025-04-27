5628f1f62f3ae5b5574eedbb00a2358446a73896
Scholtz Bálint András <scholtz.balint@gmail.com>
2025-04-27 19:45:55 +0200

    Update config.py

--- work/config.py
+++ work/config.py
@@ -1,9 +1,9 @@
 from models import LinearModel, MLPModel
 
-MODEL_PATH = "models"
-DATA_PATH = "data"
-LOG_PATH = "logs"
-RESULTS_PATH = "results"
+MODEL_PATH = "../models"
+DATA_PATH = "../data"
+LOG_PATH = "../logs"
+RESULTS_PATH = "../results"
 
 available_models = {
     'linear': LinearModel,
